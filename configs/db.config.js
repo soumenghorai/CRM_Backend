c2c19337c40249fab4ec74c897794bdfecb8bbad
Soumen Ghorai <soumenghorai2802@gmail.com>
2023-07-11 22:07:42 +0530

    adding database

--- configs/db.config.js
+++ configs/db.config.js
@@ -1,4 +1,5 @@
 module.exports = {
     DB_NAME: "crm_db",
-    DB_URL: process.env.MONGODB_URI || "mongodb://localhost/crm_db"
+    // DB_URL: process.env.MONGODB_URI || "mongodb://localhost/crm_db"
+    DB_URL: "mongodb+srv://SoumenMongo:Soumen123@crm-db.znwveec.mongodb.net/"
 }
